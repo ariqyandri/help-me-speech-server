7f1d60e655cb1d9c741658a346524be74cd55e8e
ariqyandri <ariq140199@gmail.com>
2020-11-25 16:40:33 +0100

    Add authRouter path in index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { PORT } = require("./config/constants");
+const authRouter = require("./routers/auth");
 
 const app = express();
 const bodyParserMiddleWare = express.json();
 
 app.use(bodyParserMiddleWare);
+
+app.use("/", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on Port:`, PORT);
