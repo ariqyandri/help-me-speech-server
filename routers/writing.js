d83c3b50db061059d79decde270fee812d8dc691
ariqyandri <ariq140199@gmail.com>
2020-11-27 16:02:20 +0100

    Create POST writing router

---
+++ routers/writing.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+const authMiddleware = require("../auth/middleware");
+const Writing = require("../models/").writing;
+const User = require("../models/").user;
+const router = new Router();
+
+router.post("/", authMiddleware, async (req, res, next) => {
+  try {
+    const newWriting = await Writing.create({
+      title: req.body.title,
+      content: req.body.content,
+      imageUrl: req.body.imageUrl,
+      videoUrl: req.body.videoUrl,
+      categoryId: req.body.categoryId,
+      userId: req.user.dataValues["id"],
+    });
+    res.status(200).send(newWriting);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
