2276e7e0768abbb0a1eb236bcd35eeb78211511f
ariqyandri <ariq140199@gmail.com>
2020-11-27 15:05:22 +0100

    Create relationship between user and writing

--- models/writing.js
+++ models/writing.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      writing.belongsTo(models.user);
     }
   }
   writing.init(
