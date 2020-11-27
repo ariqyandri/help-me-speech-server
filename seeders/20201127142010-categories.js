9072d48d067162d6a9aee850794c00e989b3ade4
ariqyandri <ariq140199@gmail.com>
2020-11-27 15:38:43 +0100

    Create categories seed

---
+++ seeders/20201127142010-categories.js
@@ -0,0 +1,55 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "categories",
+      [
+        {
+          name: "Speech",
+          description:
+            "A formal talk given usually to a large number of people on a special occasion.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Presentation",
+          description: "A talk giving information about something.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Interview",
+          description:
+            "A conversation in which someone is asked questions about themselves or a subject they know about for a newspaper article, television show, etc., or to see if they are suitable for a job or a course.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Poem",
+          description:
+            "A piece of writing in which the words are arranged in separate lines, often ending in rhyme, and are chosen for their sound and for the images and ideas they suggest.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Play",
+          description: "A dramatic work for the stage or to be broadcast.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Blog",
+          description: "A regular record of your thoughts, opinions, or experiences that you put on the internet for other people to read.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("categories", null, {});
+  },
+};
