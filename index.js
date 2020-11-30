dfc7b2d025f59e1c4c48c25d79395c82c4103ec6
ariqyandri <ariq140199@gmail.com>
2020-11-30 15:02:49 +0100

    Update get user's writings to new route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { PORT } = require("./config/constants");
 const corsMiddleWare = require("cors");
 const authRouter = require("./routers/auth");
 const writingRouter = require("./routers/writing");
+const writingsRouter = require("./routers/writings");
 
 const app = express();
 const bodyParserMiddleWare = express.json();
@@ -14,6 +15,8 @@ app.use(corsMiddleWare());
 app.use("/", authRouter);
 
 app.use("/writing", writingRouter);
+
+app.use("/writings", writingsRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on Port:`, PORT);
--- routers/writings.js
+++ routers/writings.js
@@ -3,14 +3,13 @@ const authMiddleware = require("../auth/middleware");
 const Writing = require("../models/").writing;
 const router = new Router();
 
-router.post("/", authMiddleware, async (req, res, next) => {
+router.get("/", authMiddleware, async (req, res, next) => {
   try {
-    const newWriting = await Writing.create({
-      ...req.body,
-      userId: req.user.dataValues["id"],
+    const myWritings = await Writing.findAll({
+      where: userId === req.user.dataValues["id"],
     });
-    console.log(req.body);
-    res.status(200).send(newWriting);
+    console.log();
+    res.status(200).send(myWritings);
   } catch (error) {
     next(error);
   }
