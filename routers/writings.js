7ae138de988a05fe51917e0d77a93281868bcf59
ariqyandri <ariq140199@gmail.com>
2020-12-01 18:54:32 +0100

    Create GET my writings according to category

--- routers/writings.js
+++ routers/writings.js
@@ -15,4 +15,19 @@ router.get("/", authMiddleware, async (req, res, next) => {
   }
 });
 
+router.get("/:category", authMiddleware, async (req, res, next) => {
+  try {
+    const myWritings = await Writing.findAll({
+      where: {
+        categoryId: req.body.categoryId,
+        userId: req.user.dataValues["id"],
+      },
+    });
+    console.log();
+    res.status(200).send(myWritings);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
