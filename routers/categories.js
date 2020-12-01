aac6d1349cb59622114075816c74f848ff5f31b4
ariqyandri <ariq140199@gmail.com>
2020-12-01 19:13:56 +0100

    Remove auth middleware on get categories router

--- routers/categories.js
+++ routers/categories.js
@@ -1,9 +1,8 @@
 const { Router } = require("express");
-const authMiddleware = require("../auth/middleware");
 const Categories = require("../models").category;
 const router = new Router();
 
-router.get("/", authMiddleware, async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const categories = await Categories.findAll();
     res.status(200).send(categories);
